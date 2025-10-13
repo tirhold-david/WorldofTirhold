287de3270a0c2d310f7e6484bf44119d3f17818a
tirhold.david.2022i_ <tirhold.david.2022i@bankitatabanya.hu>
2025-10-13 10:24:40 +0200

    first commit

---
+++ src/Game.java
@@ -0,0 +1,7 @@
+public class Game {
+    public static void main(String[] args)
+    {
+        //git remote add origin URL
+        //git remote add origin https://github.com/tirhold-david/WordofTirhold.git
+    }
+}
